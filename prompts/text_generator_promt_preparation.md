0b1275f2dbddf7067b952d330a4e07110d1ab03b
miguelvime <miguelvicentemeso@gmail.com>
2026-06-06 20:13:57 +0200

    Creo Script calculadora de alfa e input de LLM generador

---
+++ prompts/text_generator_promt_preparation.md
@@ -0,0 +1,4 @@
+Ideas a incluir:
+    - **Rol**: eres un médico rehabilitador. En vez de pacientes la información que tienes son sus códigos CIF. Tu tarea es escribir 1 historia clínica realista, como si fueras el médico rehabilitador, a partir de la información disponible. Consulta @RAG/icf_core_set_chronic_widespread_pain_brief.pdf para tener información de en qué casos se asigna cada código.
+    - 
+
--- scripts/alfa_calc.R
+++ scripts/alfa_calc.R
@@ -4,3 +4,24 @@
 library(icr)
 
 # Example data: 5 raters, 10 subjects
+data<-icr::codings
+alpha<-icr::krippalpha(codings, 
+                metric = 'nominal',
+                bootstrap = TRUE,
+                bootnp = TRUE,
+                cores = 4)
+
+plot(alpha)
+
+df <- plot(alpha, return_data = TRUE)
+
+library(ggplot2)
+ggplot() +
+  geom_line(data = df[df$ci_limit == FALSE, ], aes(x, y, color = type)) +
+  geom_area(data = df[df$ci == TRUE, ], aes(x, y, fill = type), alpha = 0.4) +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(legend.position = "bottom", legend.title = element_blank()) +
+  ggtitle(expression(paste("Bootstrapped ", alpha))) +
+  xlab("value") + ylab("density") +
+  guides(fill = FALSE)
